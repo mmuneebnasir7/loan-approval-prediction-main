95832ccb3b0b30841661d1378a3592d35541874f
mmuneebnasir7 <mmuneebnasir7@gmail.com>
2025-08-06 15:45:59 +0500

    Update app.py

--- app.py
+++ app.py
@@ -123,5 +123,5 @@ def predict():
 		# Return error
 		return render_template('error.html', prediction = prediction)
 
-if __name__ == '__main__':
-    app.run(debug = True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080, debug=False, use_reloader=False)
